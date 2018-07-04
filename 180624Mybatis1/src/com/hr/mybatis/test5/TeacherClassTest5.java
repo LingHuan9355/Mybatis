2bb274b837c35c3e587a1b9835763c06a284ac01
MrLing <LingHuan5593@qq.com>
2018-07-05 00:18:26 +0800

    实现关联表查询:一对一关联,根据班级id查询班级信息(带老师的信息)

---
+++ 180624Mybatis1/src/com/hr/mybatis/test5/TeacherClassTest5.java
@@ -0,0 +1,45 @@
+package com.hr.mybatis.test5;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.hr.mybatis.beans.Classes;
+import com.hr.mybatis.beans.Order;
+import com.hr.mybatis.beans.User;
+import com.hr.mybatis.util.MyBatisUtils;
+import com.hr.mybatis.xml.test2.UserTest2;
+
+/**
+ * 
+ * @Name  : TeacherClassTest5
+ * @Author : LH
+ * @Date : 2018年7月4日 下午11:45:42
+ * @Version : V1.0
+ * 
+ * @Description :   测试: 一对一关联表查询
+ */
+public class TeacherClassTest5 {
+
+			@Test
+			public void testClass() throws IOException {
+					SqlSessionFactory sqlSessionFactory = MyBatisUtils.getFactory();
+					SqlSession sqlSession = sqlSessionFactory.openSession(true);
+					
+					String statement = "com.hr.mybatis.test5.ClassMapper" + ".getClass";
+					 statement = "com.hr.mybatis.test5.ClassMapper" + ".getClass2";
+					
+					Classes classes = sqlSession.selectOne(statement, 1);
+					System.out.println(classes);
+					sqlSession.close();
+					
+			}
+
+}
